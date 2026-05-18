5395c780fd4dbdab45ec40181722615a5bb188d2
mdbahauddinb101 <mdbahauddinb101@gmail.com>
2026-05-18 21:31:20 +0600

    navbar created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Components/Header'
+
+
+const App = () => {
+  return (
+   <>
+   <Header/>
+   </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+
+
+const Header = () => {
+  return (
+    <>
+    <header id="header">
+        <div className="container">
+           <div className="flex items-center">
+            <img src="/public/logo.png" alt="logo" />
+            <ul className="flex ml-8 gap-8">
+                <li>Home</li>
+                <li>Categories</li>
+                <li>Deals</li>
+                <li>About</li>
+            </ul>
+            <div className="relative ml-20">
+                <input className="relative w-lg h-10.5 rounded-lg border border-[#D1D5DB] placeholder:p-7" type="text" placeholder="Search products..."/>
+                <img className="absolute top-3.25 left-1.5" src="/public/search.png" alt="" />
+            </div>
+            <div className="flex gap-4 ml-20">
+                <img className="object-contain" src="/public/cart.png" alt="" />
+                <img className="object-contain"src="/public/profile.png" alt="" />
+            </div>
+           </div>
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default Header
