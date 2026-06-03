43fef6c0a3521f759d1d899e61151a91b3a67611
mdbahauddinb101 <mdbahauddinb101@gmail.com>
2026-06-03 09:12:06 +0600

    ALL RELOADED

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -5,14 +5,17 @@ const Featured = () => {
    <>
    <section id="featured">
     <div className="container">
-        <h2>Featured</h2>
-        <div className="flex gap-6">
+        <div className="featured-header">
+        <h2 className="font-bold text-[30px] leading-9">Featured Products</h2>
+        <button className="text-[#3B82F6] font-medium text-[16px] leading-6">View All</button>
+        </div>
+        <div className="flex justify-between items-center gap-6">
              <div className="featured-card">
-                <img src="/public/headphone.png" alt="" />
-                <h3>Headphones</h3>
-                <p>Premium quality sound</p>
+                <img className="mb-4" src="/public/headphone.png" alt="" />
+                <h3 className="font-semibold text-[16px] mb-1.5 ">Headphones</h3>
+                <p className="font-normal text-[14px] text-[#4B5563] mb-2.75">Premium quality sound</p>
                 <div className="value-container">
-                    <span className="price">$99.99</span>
+                    <span className="font-bold text-[20px] leading-7 text-[#111827]">$99.99</span>
                     <img src="/public/button.png" alt="" />
                 </div>
              </div>
